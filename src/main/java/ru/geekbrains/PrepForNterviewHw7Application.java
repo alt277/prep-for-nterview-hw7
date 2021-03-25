45e323edf6b64d4f0cbba020bae3851e61bed84a
alt277 <altima875@gmail.com>
2021-03-25 17:31:17 +0400

    Initial commit

---
+++ src/main/java/ru/geekbrains/PrepForNterviewHw7Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrepForNterviewHw7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrepForNterviewHw7Application.class, args);
+    }
+
+}
